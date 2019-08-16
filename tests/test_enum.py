a049d8bd58c346eb40f01679f00690b248960cde
Henrik Holmboe <henrik@holmboe.se>
2019-08-16 15:35:20 +0200

    Test our cutom enum

---
+++ tests/test_enum.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# 3rd party imports
+import pytest
+
+
+def test_import_status():
+    try:
+        from phabfive.constants import Status
+    except ImportError:
+        pytest.fail("Unexpected ImportError")
+
+
+def test_status_enum_value():
+    """This tests that the __str__() method is returning our value as intended."""
+    from phabfive.constants import Status
+
+    assert Status.ACTIVE == "active"
+    assert Status.INACTIVE == "inactive"
