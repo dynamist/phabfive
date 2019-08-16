cba85f2c3096265cceca116c0cab475364cda13c
Henrik Holmboe <henrik@holmboe.se>
2019-08-16 16:20:09 +0200

    Add missing import

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -5,5 +5,7 @@ import pytest
 
 
 def test_status_choices():
+    from phabfive.constants import REPO_STATUS_CHOICES
+
     assert "active" in REPO_STATUS_CHOICES
     assert "inactive" in REPO_STATUS_CHOICES
