29ade17c3f36cd8144fd2d2ed488611d4ea2d8fb
maiyuki <mai@dynamist.se>
2019-10-07 12:24:46 +0200

    Rename Display and IO lists in constants.py also

--- phabfive/constants.py
+++ phabfive/constants.py
@@ -5,8 +5,8 @@ from enum import Enum, auto
 # https://secure.phabricator.com/w/object_name_prefixes/
 MONOGRAMS = {"diffusion": "R[0-9]+", "passphrase": "K[0-9]+", "paste": "P[0-9]+"}
 # IO_EDIT_URI_VALUES = ["default", "read", "write", "never"]
-IO_NEW_URI_VALUES = ["default", "observe", "mirror", "never"]
-DISPLAY_VALUES = ["default", "always", "hidden"]
+IO_NEW_URI_CHOICES = ["default", "observe", "mirror", "never"]
+DISPLAY_CHOICES = ["default", "always", "hidden"]
 
 class EnumAutoNameLowerCase(Enum):
     def _generate_next_value_(name, start, count, last_values):
