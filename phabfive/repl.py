d0660823d4b95d93e7397d0999641430129e5f78
Johan Andersson <johan@dynamist.se>
2020-06-18 11:24:59 +0200

    Add new debug feature where you can enter a simple prepped REPL where you have easy access to the API where you can run code directly against the API.

---
+++ phabfive/repl.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# python std lib
+import logging
+
+# phabfive imports
+from phabfive.core import Phabfive
+from phabfive.exceptions import PhabfiveRemoteException
+
+# 3rd party imports
+from phabricator import APIError
+
+
+log = logging.getLogger(__name__)
+
+
+class Repl(Phabfive):
+    def __init__(self):
+        super(Repl, self).__init__()
+
+    def run(self):
+        print("*************")
+        print("use self.phab to access the phacility API")
+        print("use self.conf to access current client configuration")
+        print("use pp() to prettyprint the API response back from self.phab.* calls")
+        print("*************")
+
+        from pprint import pprint as pp
+
+        import pdb
+        pdb.set_trace()
