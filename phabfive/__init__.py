e838d9cb9ef8b2e2bbcb4457ddc0ca34a25bf348
Johan Andersson <johan@dynamist.se>
2023-01-04 10:07:53 +0100

    First iteration of implementing "maniphest create" cli command
    Update and implement proper logging facility for the phabfive logging namespace
    Add cli option --log-level=<level> to allow to change loglevel from cli, remove the -vvvvv capability
    Code lint most of the cli.py file
    Minor u pdates to test docker compose file
    Add new example template that can be used during development and testing of the bulk create feature
    Added jinja2 as new dependency to allow to template the tickets title and description when bulk creating them

--- phabfive/__init__.py
+++ phabfive/__init__.py
@@ -1,6 +1,50 @@
 # -*- coding: utf-8 -*-
 
+# python stdlib
+import logging
+import os
+import sys
+
 __author__ = "Rickard Eriksson"
 __email__ = "rickard@dynamist.se"
 __version__ = "0.2.0"
 __url__ = "https://github.com/dynamist/phabfive"
+
+
+def init_logging(log_level):
+    """
+    Init logging settings with default set to INFO
+    """
+    _log_level = logging.getLevelName(log_level)
+
+    if isinstance(_log_level, str):
+        print("CRITICAL: Undefined log-level set, please use any of the defined log levels inside Python logging module")
+        sys.exit(1)
+
+    if log_level == "DEBUG":
+        msg = "%(levelname)s - %(name)s:%(lineno)s - %(message)s"
+    else:
+        msg = "%(levelname)s - %(message)s"
+
+    logging_conf = {
+        "version": 1,
+        "root": {
+            "level": log_level,
+            "handlers": ["console"],
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "level": log_level,
+                "formatter": "simple",
+                "stream": "ext://sys.stdout",
+            },
+        },
+        "formatters": {
+            "simple": {
+                "format": f"{msg}",
+            },
+        },
+    }
+
+    logging.config.dictConfig(logging_conf)
