f1709d45e327970feb6978382596cb04e03398dc
Titus <titus.hoon@thehoons.org>
2023-04-07 20:10:53 +0800

    Added Owner, Subscriber, Priority onto maniphest

--- phabfive/maniphest.py
+++ phabfive/maniphest.py
@@ -55,7 +55,24 @@ class Maniphest(Phabfive):
         # STEP: Data structure validation, check title + description, projects existance
 
         ### Validate all projects is correct data types and exists in Phabricator instance
-        projects_query = self.phab.project.search(constraints={"name": ""})
+
+        #projects_query = self.phab.project.search(constraints={"name": ""})
+        #Upgrade to handle phab instances with more than 100 project/tags
+
+        #create a new construct to consolidate all project/tags
+        projects_query = {}
+        projects_query["data"] = []
+
+        r = self.phab.project.search(constraints={"name": ""})
+        length = len(r.data)
+        a = r['cursor']['after']
+        projects_query["data"].extend(r.data)
+
+        while (length >= 100 and a is not None):
+            r = self.phab.project.search(constraints={"name": ""},after=a)
+            length = len(r.data)
+            a = r['cursor']['after']
+            projects_query["data"].extend(r.data)
 
         project_name_to_id_mapping = {
             project["fields"]["name"]: project["phid"]
@@ -81,10 +98,98 @@ class Maniphest(Phabfive):
                     has_errors = True
 
             if has_errors:
-                log.error(f"Could not find one or more specified projects in phabricator instance. All available project names")
+                log.error(f"Could not find one or more specified users, projects in phabricator instance. All available project names")
                 log.error(list(project_name_to_id_mapping.keys()))
+                log.error(f"All available users names")
+                log.error(list(user_name_to_id_mapping.keys()))
                 return (False, None)
 
+        #create a new construct to consolidate all users for owner and subscriber
+        users_query = {}
+        users_query["data"] = []
+
+        r = self.phab.user.search()
+        length = len(r.data)
+        afterCursor = r['cursor']['after']
+        users_query["data"].extend(r.data)
+
+        while (length >= 100 and afterCursor is not None):
+            r = self.phab.user.search(
+                after=afterCursor)
+            length = len(r.data)
+            afterCursor = r['cursor']['after']
+            users_query["data"].extend(r.data)
+
+        user_name_to_id_mapping = {
+            user["fields"]["username"]: user["phid"]
+            for user in users_query["data"]
+        }
+
+        log.debug("All Phabricator user names")
+        log.debug(user_name_to_id_mapping)
+
+
+        '''NEED TO HANDLE Subscriber, Owner'''
+        for index, ticket_data in enumerate(data["tickets"]):
+            if ("owner" not in ticket_data):
+                break
+
+        ####?????
+        #    if not isinstance(ticket_data["owner"], list):
+        #        log.error(f"data key 'owner' must be of list type")
+        #        return (False, None)
+        #        print(f"data key 'owner' must be of list type")
+
+            has_errors = False
+
+            if ticket_data["owner"] not in user_name_to_id_mapping:
+                log.error(f"Owner User name '{ticket_data['owner']}' not found in Phabricator instance")
+                has_errors = True
+
+            if has_errors:
+                log.error(f"Could not find one or more specified users, projects in phabricator instance. All available project names")
+                log.error(list(user_name_to_id_mapping.keys()))
+                log.error(f"All available users names")
+                log.error(list(user_name_to_id_mapping.keys()))
+                return (False, None)
+
+        for index, ticket_data in enumerate(data["tickets"]):
+            if ("subscribers" not in ticket_data):
+                break
+
+            if not isinstance(ticket_data["subscribers"], list):
+                log.error(f"data key '{subscriber}' must be of list type")
+                return (False, None)
+
+            has_errors = False
+            
+            for subscriber in ticket_data["subscribers"]:
+
+                if subscriber not in user_name_to_id_mapping:
+                    log.error(f"Subscriber user name '{subscriber}' not found in Phabricator instance")
+                    has_errors = True
+
+            if has_errors:
+                log.error(f"Could not find one or more specified users, projects in phabricator instance. All available project names")
+                log.error(list(project_name_to_id_mapping.keys()))
+                log.error(f"All available users names")
+                log.error(list(user_name_to_id_mapping.keys()))
+                return (False, None)
+
+        ##### PRIORITY
+        for index, ticket_data in enumerate(data["tickets"]):
+            if ("priority" not in ticket_data):
+                break
+            if ticket_data["priority"] not in TICKET_PRIORITIES:
+                log.error(f"Priority '{ticket_data['priority']}' not found in Phabricator instance")
+                has_errors = True
+
+            if has_errors:
+                log.error(f"Could not find one or more specified users, projects in phabricator instance. All available project names")
+                log.error(list(project_name_to_id_mapping.keys()))
+                log.error(f"All available users names")
+                log.error(list(user_name_to_id_mapping.keys()))
+                return (False, None)
         #####
         # STEP: Render
         log.debug(data)
@@ -98,8 +203,18 @@ class Maniphest(Phabfive):
             rendered_data.append({
                 "title": Template(ticket_data["title"]).render(variables),
                 "description": Template(ticket_data["description"]).render(variables),
-                "projects": ticket_data["projects"],
+
             })
+
+            r = len(rendered_data)
+            if ("owner" in ticket_data):
+                rendered_data[r-1]["owner"] = ticket_data["owner"]
+            if ("priority" in ticket_data):
+                rendered_data[r-1]["priority"] = ticket_data["priority"]
+            if ("projects" in ticket_data):
+                rendered_data[r-1]["projects"] = ticket_data["projects"]
+            if ("subscribers" in ticket_data):
+                rendered_data[r-1]["subscribers"] = ticket_data["subscribers"]
 
         log.debug(rendered_data)
 
@@ -124,8 +239,14 @@ class Maniphest(Phabfive):
             transactions = [
                 {"type": "title", "value": ticket_data["title"]},
                 {"type": "description", "value": ticket_data["description"]},
-                {"type": "priority", "value": ticket_data.get("priority", TICKET_PRIORITY_NORMAL)},
             ]
+
+            if ticket_data.get("priority"):
+                transactions.append({
+                    "type": "priority", "value": ticket_data["priority"]},)
+            else:
+                transactions.append({
+                    "type": "priority", "value": ticket_data.get("priority", TICKET_PRIORITY_NORMAL)},)
 
             if ticket_data.get("projects"):
                 transactions.append({
@@ -134,6 +255,22 @@ class Maniphest(Phabfive):
                         project_name_to_id_mapping[project_name]
                         for project_name in ticket_data["projects"]
                     ],
+                })
+
+            if ticket_data.get("subscriber"):
+                transactions.append({
+                    "type": "subscribers.add",
+                    "value": [
+                        user_name_to_id_mapping[subscriber]
+                        for subscriber in ticket_data["subscribers"]
+                    ],
+                })
+
+            if ticket_data.get("owner"):
+                transactions.append({
+                    "type": "owner",
+                    "value": 
+                        user_name_to_id_mapping[ticket_data["owner"]],
                 })
 
             log.debug("transactions for ticket")
