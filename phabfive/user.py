55a21a62367e0269210ebdeab7fa17ce37f24ccf
Henrik Holmboe <henrik@dynamist.se>
2026-01-12 03:09:01 +0100

    refactor: Improve method naming across modules
    - create_task_cli -> create_task (not CLI-specific)
    - create_from_config -> create_tasks_from_yaml (more specific)
    - get_whoami -> whoami (cleaner API)
    - add_comment -> add_task_comment (more specific)
    - info -> get_task_info (was too vague)
    - days_to_unix -> days_ago_to_timestamp (clearer intent)
    - _load_search_from_yaml -> _load_search_config (simpler)

--- phabfive/user.py
+++ phabfive/user.py
@@ -3,13 +3,12 @@
 # python std lib
 import logging
 
-# phabfive imports
-from phabfive.core import Phabfive
-from phabfive.exceptions import PhabfiveRemoteException
-
 # 3rd party imports
 from phabricator import APIError
 
+# phabfive imports
+from phabfive.core import Phabfive
+from phabfive.exceptions import PhabfiveRemoteException
 
 log = logging.getLogger(__name__)
 
@@ -18,7 +17,7 @@ class User(Phabfive):
     def __init__(self):
         super(User, self).__init__()
 
-    def get_whoami(self):
+    def whoami(self):
         """Return filtered user info dict with userName, realName, primaryEmail, uri."""
         try:
             response = self.phab.user.whoami()
